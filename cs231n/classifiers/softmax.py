551abdd3d721424bf5a2acbb63a0592a781a3bfb
David Corvoysier <david.corvoysier@gmail.com>
2017-03-16 11:02:58 +0100

    softmax: implemented naive gradient

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -46,12 +46,35 @@ def softmax_loss_naive(W, X, y, reg):
         sum_e_scores += np.exp(shifted_scores[j])
       # Loss is increased by the log of the sum minus the correct class score
       loss += np.log(sum_e_scores) - shifted_scores[y[i]]
+      # Explaination for the calculus of the gradient
+      #     L = log(sum(exp(s))) - s[y[i]]
+      # Derivative of neperian log; ln(f)' = f'/f
+      # d[j]L = d[j]sum(exp(s))/sum(exp(s)) - d[j]s[y[i]]
+      # Derivative of sum: (f + g)' = f' + g'
+      # d[j]L = sum(d[j]exp(s))/sum(exp(s)) - d[j]s[y[i]]
+      # Derivative of exp: exp(f)' = f'.exp(f)
+      # d[j]L = sum(d[j]s.exp(s))/sum(exp(s) - d[j]s[y[i]]
+      # With, for a given class c: s[c] = w[c].X[i]
+      #   d[j]s[c] = 0 if j <> c
+      #   d[c]s[c] = X[i]
+      # Then, in the first term of the gradient, all elements of the sum are
+      # null BUT the one where j = k, and the second term of the gradient is
+      # null except it j = y[i]
+      #    d[j]L = X[i].exp(s[j])/sum(exp(s)) if j <> y[i]
+      # d[y[i]]L = X[i].exp(s[j])/sum(exp(s)) -X[i]
+      # Calculate gradient common part for each class
+      for j in range(0, num_classes):
+          dW[:,j] += X[i] * np.exp(shifted_scores[j])/sum_e_scores
+      # For the correct class, the gradient has an extra term
+      dW[:,y[i]] -= X[i]
 
   # We want mean values
   loss /= num_train
+  dW /= num_train
 
   # Add regression
   loss += 0.5 * reg * np.sum(W*W)
+  dW += reg * W
 
   #############################################################################
   #                          END OF YOUR CODE                                 #
