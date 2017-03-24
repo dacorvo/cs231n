dd17ae9639a66ad48a677d1c0f473fec3c951209
David Corvoysier <david.corvoysier@gmail.com>
2017-03-24 11:12:24 +0100

    features: adapt to python3

--- cs231n/features.py
+++ cs231n/features.py
@@ -41,14 +41,14 @@ def extract_features(imgs, feature_fns, verbose=False):
   imgs_features[0] = np.hstack(first_image_features).T
 
   # Extract features for the rest of the images.
-  for i in xrange(1, num_images):
+  for i in range(1, num_images):
     idx = 0
     for feature_fn, feature_dim in zip(feature_fns, feature_dims):
       next_idx = idx + feature_dim
       imgs_features[i, idx:next_idx] = feature_fn(imgs[i].squeeze())
       idx = next_idx
     if verbose and i % 1000 == 0:
-      print 'Done extracting features for %d / %d images' % (i, num_images)
+      print('Done extracting features for %d / %d images' % (i, num_images))
 
   return imgs_features
 
@@ -115,7 +115,8 @@ def hog_feature(im):
     # select magnitudes for those orientations
     cond2 = temp_ori > 0
     temp_mag = np.where(cond2, grad_mag, 0)
-    orientation_histogram[:,:,i] = uniform_filter(temp_mag, size=(cx, cy))[cx/2::cx, cy/2::cy].T
+    orientation_histogram[:,:,i] = \
+    uniform_filter(temp_mag,size=(cx,cy))[int(cx/2)::cx, int(cy/2)::cy].T
   
   return orientation_histogram.ravel()
 
