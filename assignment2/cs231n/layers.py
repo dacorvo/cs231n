79c9dc789c6194704de0e052a43b11eed679435b
David Corvoysier <david.corvoysier@gmail.com>
2017-03-29 16:26:39 +0200

    batch norm: implement forward train

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -180,7 +180,17 @@ def batchnorm_forward(x, gamma, beta, bn_param):
     # the momentum variable to update the running mean and running variance,    #
     # storing your result in the running_mean and running_var variables.        #
     #############################################################################
-    pass
+    # Evaluate sample mean
+    sample_mean = np.mean(x, axis=0)
+    # Evaluate sample variance (note: broadcast on - )
+    sample_var = np.mean(np.square(x - sample_mean), axis=0)
+    # Normalize the input (note: triple broadcast on - , +, then /)
+    xn = (x - sample_mean)/(np.sqrt(sample_var + eps))
+    # And scale to produce the output (note: broadcast on +)
+    out = xn * gamma + beta
+    # Update running mean and variance
+    running_mean = momentum * running_mean + (1 - momentum) * sample_mean
+    running_var = momentum * running_var + (1 - momentum) * sample_var
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
